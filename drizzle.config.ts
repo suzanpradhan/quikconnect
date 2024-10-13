38111b22171b57b1ab3161fef5af2a01e70207d6
subarnaad <133129998+subarnaad@users.noreply.github.com>
2024-10-13 13:10:41 +0545

    git updated

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,3 @@
-// /* eslint-disable @typescript-eslint/no-non-null-assertion */
-// import { defineConfig } from 'drizzle-kit';
-// import 'dotenv/config';
-
-// export default defineConfig({
-//   schema: './src/db/schema.ts',
-//   out: './src/drizzle/migrations',
-//   dialect: 'postgresql',
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!,
-//   },
-//   verbose: true,
-//   strict: true,
-// });
-
 import { defineConfig } from 'drizzle-kit';
 // via connection params
 export default defineConfig({
