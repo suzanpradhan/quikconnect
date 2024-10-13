bbd7a27353028af3c36c5572a54a90138453ac66
subarnaad <133129998+subarnaad@users.noreply.github.com>
2024-10-13 12:48:13 +0545

    eslint used

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,14 +1,11 @@
-import {migrate} from "drizzle-orm/node-postgres/migrator"
-import db from "../db/db_connect"
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import db from '../db/db_connect';
 
-
-
- async function migrationData() {
-        await migrate(db,{migrationsFolder:"./src/drizzle/migrations"})
-        process.exit(0)
-
- }
-migrationData().catch((e)=>{
-        console.log(e);
-        process.exit(1)
-})
+async function migrationData() {
+  await migrate(db, { migrationsFolder: './src/drizzle/migrations' });
+  process.exit(0);
+}
+migrationData().catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
