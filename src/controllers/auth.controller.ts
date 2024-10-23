c0b3a02d15a205f954d14361c7655060ce449a4a
subarnaad <133129998+subarnaad@users.noreply.github.com>
2024-10-23 21:26:20 +0545

    signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,18 +1,26 @@
-import { UserTable } from '@/schema/schema';
+import { UserTable } from '../schema/schema';
 import bcrypt from 'bcryptjs';
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
 import { db } from '../migrate';
+import { eq } from 'drizzle-orm';
+import generateToken from '@/utils/generate.token';
 
 export const signup = async (req: Request, res: Response) => {
   try {
     const { name, email, password, conformPassword } = req.body;
     if (!name || !email || !password || !conformPassword) {
-      return res.status(400).json({ Error: 'fill all feilds' });
+      res.status(400).json({ Error: 'fill all feilds' });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ Error: 'Password must be at least 6 characters long' });
     }
     if (password !== conformPassword) {
       return res.status(400).json({ Error: "password don't match! use same password as before" });
     }
-
+    const checkUserExist = await db.select().from(UserTable).where(eq(UserTable.email, email)).limit(1);
+    if (checkUserExist) {
+      return res.status(400).json({ message: 'mail already exist' });
+    }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -20,13 +28,59 @@ export const signup = async (req: Request, res: Response) => {
       name,
       email,
       password: hashedPassword,
-      conformPassword,
+      conformPassword: hashedPassword,
     });
     if (newUser) {
-      return res.status(200).json({ message: 'user created' });
+      res.status(200).json({ message: 'user created' });
+      // generateToken(newUser.id,res)
+      // res.status(201).json({
+      //   id:newUser.id,
+      //   name:newUser.name,
+      // })
     }
   } catch (error) {
     console.error(signup, error);
     res.status(500).json({ message: 'internal server error' });
+  }
+};
+
+export const signin = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide both email and password' });
+    }
+
+    const getUsermail = await db.select().from(UserTable).where(eq(UserTable.email, email)).limit(1);
+    if (!getUsermail) {
+      res.status(400).json({ message: 'invalid credential' });
+    }
+
+    const user = getUsermail[0]; // getUsermail ma arry ko form ma basxa data so
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ Message: 'invalid credential' });
+    }
+    res.status(200).json({
+      message: 'Signin successful',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error: any) {
+    console.error('error in controller signin', error);
+    return res.status(400).json({ message: 'internal server error' });
+  }
+};
+
+export const logout = async (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    
   }
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
-import { signup } from '../controllers/auth.controller';
+import { signup, signin } from '../controllers/auth.controller';
 import express from 'express';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signup);
+authRouter.post('/signin', signin);
 
 // authRouter.get('/signup', (req, res) => {
 //   res.send('signup route is running');
