78d0fb3cf5c080d81fa5798d04f3e2610892d4f7
subarnaad <133129998+subarnaad@users.noreply.github.com>
2024-11-26 16:38:02 +0545

    schema setup for socket

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,15 +1,46 @@
-import { bigint, varchar, uuid, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { bigint, boolean, varchar, uuid, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const UserTable = pgTable('user', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: varchar('name').notNull(),
   email: varchar('email', { length: 255 }).notNull(),
   password: varchar('password').notNull(),
-  // confirmPassword: varchar('confirmPassword').notNull(),
   resetToken: text('resetToken'),
   resetTokenExpiry: timestamp('resetTokenExpiry'),
   nameUpdateAt: timestamp('nameUpdateAt'),
   phoneNumber: bigint({ mode: 'number' }),
   gender: varchar('gender'),
   avatar: varchar('avatar'),
+});
+
+export const ChatTable = pgTable('ChatTable', {
+  chatId: uuid('chatId').primaryKey(),
+  name: varchar('name'),
+  isGroupChat: boolean('isGroupChat').default(false),
+  createdAt: timestamp('createdAt').defaultNow(),
+});
+
+export const ChatMembersTable = pgTable('ChatMember', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  chatId: uuid('chatId')
+    .notNull()
+    .references(() => ChatTable.chatId),
+  userId: uuid('userId')
+    .notNull()
+    .references(() => UserTable.id),
+  isAdmin: boolean('isAdmin').default(false),
+  joinedAt: timestamp('joinedAt').defaultNow(),
+});
+
+export const MessageTable = pgTable('Messages', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  chatId: uuid('chatId')
+    .notNull()
+    .references(() => ChatTable.chatId),
+  senderId: uuid('senderId')
+    .notNull()
+    .references(() => UserTable.id),
+  content: text('content').notNull(),
+  messageType: varchar('messageType').default('text'),
+  createdAt: timestamp('createdAt').defaultNow(),
 });
