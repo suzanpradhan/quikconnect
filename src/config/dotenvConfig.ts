99b0e739d5b029e8fbb1fec6f3a9eeeb89c7020b
subarnaad <133129998+subarnaad@users.noreply.github.com>
2024-12-28 19:50:57 +0545

    fix:add rootDir and upadate scripts

--- src/config/dotenvConfig.ts
+++ src/config/dotenvConfig.ts
@@ -1,34 +1,15 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-export const CONFIG = {
-  PORT: process.env.PORT || 3000,
-  HOST: process.env.HOST || 'http://192.168.1.9',
-  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
-  BASE_URL: `${process.env.HOST || 'http://192.168.1.9'}:${process.env.PORT || 3000}`,
-  SWAGGER_PATH: process.env.SWAGGER_PATH || '/api-docs',
-  UPLOAD_DIR: process.env.UPLOAD_DIR || 'uploads/avatars/',
-  UPLOAD_DIR_Messsage: process.env.UPLOAD_DIR_Messsage || 'uploads/messageFileUploads',
-
-  MAX_FILE_SIZE: parseInt(process.env.MAX_FILE_SIZE || '5000000', 10),
-  JWT_SECRET: process.env.JWT_SECRET || 'quickconnectsecretkey',
-  LOG_LEVEL: process.env.LOG_LEVEL || 'info', // Set default logging level
-  EMAIL_USER: process.env.EMAIL_USER,
-  EMAIL_PASS: process.env.EMAIL_PASS,
-  CLIENTRESET_URL: process.env.CLIENTRESET_URL,
-  DATABASE_URL: process.env.DATABASE_URL,
-};
-
 // import dotenv from 'dotenv';
 
 // dotenv.config();
 // export const CONFIG = {
-//   PORT: process.env.PORT || 8001,
-//   HOST: process.env.HOST || 'https://localhost',
+//   PORT: process.env.PORT || 3000,
+//   HOST: process.env.HOST || 'http://192.168.1.9',
 //   CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
-//   BASE_URL: `${process.env.HOST || 'https://localhost'}:${process.env.PORT || 8001}`,
+//   BASE_URL: `${process.env.HOST || 'http://192.168.1.9'}:${process.env.PORT || 3000}`,
 //   SWAGGER_PATH: process.env.SWAGGER_PATH || '/api-docs',
 //   UPLOAD_DIR: process.env.UPLOAD_DIR || 'uploads/avatars/',
+//   UPLOAD_DIR_Messsage: process.env.UPLOAD_DIR_Messsage || 'uploads/messageFileUploads',
+
 //   MAX_FILE_SIZE: parseInt(process.env.MAX_FILE_SIZE || '5000000', 10),
 //   JWT_SECRET: process.env.JWT_SECRET || 'quickconnectsecretkey',
 //   LOG_LEVEL: process.env.LOG_LEVEL || 'info', // Set default logging level
@@ -37,3 +18,23 @@ export const CONFIG = {
 //   CLIENTRESET_URL: process.env.CLIENTRESET_URL,
 //   DATABASE_URL: process.env.DATABASE_URL,
 // };
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const CONFIG = {
+  PORT: process.env.PORT || 3000,
+  HOST: process.env.HOST || 'https://localhost',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
+  BASE_URL: `${process.env.HOST || 'https://localhost'}:${process.env.PORT || 3000}`,
+  SWAGGER_PATH: process.env.SWAGGER_PATH || '/api-docs',
+  UPLOAD_DIR: process.env.UPLOAD_DIR || 'uploads/avatars/',
+  UPLOAD_DIR_Messsage: process.env.UPLOAD_DIR_Messsage || 'uploads/messageFileUploads',
+  MAX_FILE_SIZE: parseInt(process.env.MAX_FILE_SIZE || '5000000', 10),
+  JWT_SECRET: process.env.JWT_SECRET || 'quickconnectsecretkey',
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info', // Set default logging level
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  CLIENTRESET_URL: process.env.CLIENTRESET_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,34 @@
-// import express from 'express';
-// import cors from 'cors';
-// import authRoute from './routes/auth.route';
-// import userInfoRoute from './routes/userInfo.route';
-// import chatRoute from './routes/chat.route';
-// import { Server } from 'socket.io';
-// import { setupSwagger } from './swagger';
-// import { CONFIG } from './config/dotenvConfig';
-// import { socketLogic } from './socketLogic';
-// import { instrument } from '@socket.io/admin-ui';
-// import fs from 'fs';
-// import https from 'https';
-
-// const app = express();
-
-// const sslOptions = {
-//   key: fs.readFileSync('server.key'), // Path to your private key
-//   cert: fs.readFileSync('server.cert'), // Path to your certificate
-// };
-// const server = https.createServer(sslOptions, app); //
-// export const io = new Server(server, {
-//   cors: {
-//     methods: ['GET', 'POST'],
-//     credentials: true,
-//     origin: ['https://admin.socket.io', CONFIG.BASE_URL],
-//   },
-// });
-
-// instrument(io, {
-//   auth: false,
-//   mode: 'development',
-// });
-
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json()); //express default middleware
-
-// app.set('socket', io);
-// socketLogic(io);
-
-// app.use('/api', authRoute, userInfoRoute);
-// app.use('/api', chatRoute);
-
-// setupSwagger(app, `${CONFIG.BASE_URL}/api`);
-
-// server.listen(CONFIG.PORT, () => {
-//   console.log(`server is running on ${CONFIG.BASE_URL}`);
-//   console.log(`get api from ${CONFIG.BASE_URL}/api-docs`);
-// });
-
 import express from 'express';
 import cors from 'cors';
 import authRoute from './routes/auth.route';
 import userInfoRoute from './routes/userInfo.route';
 import chatRoute from './routes/chat.route';
-import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import { setupSwagger } from './swagger';
 import { CONFIG } from './config/dotenvConfig';
 import { socketLogic } from './socketLogic';
-import { Response } from 'express';
+import { instrument } from '@socket.io/admin-ui';
+import fs from 'fs';
+import https from 'https';
 
 const app = express();
 
-const server = createServer(app);
+const sslOptions = {
+  key: fs.readFileSync('server.key'), // Path to your private key
+  cert: fs.readFileSync('server.cert'), // Path to your certificate
+};
+const server = https.createServer(sslOptions, app); //
 export const io = new Server(server, {
   cors: {
     methods: ['GET', 'POST'],
     credentials: true,
-    // origin: '*',
-    origin: [
-      '*',
-      'wss://192.168.1.7:8001',
-      'ws://192.168.1.7:8001',
-      'http://127.0.0.1:5501',
-      'http://192.168.1.7:8001',
-      'http://localhost:3000',
-      'http://127.0.0.1:5500',
-      'http://127.0.0.1:5501/',
-      CONFIG.BASE_URL,
-    ],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['https://admin.socket.io', CONFIG.BASE_URL],
   },
+});
+
+instrument(io, {
+  auth: false,
+  mode: 'development',
 });
 
 app.use(cors());
@@ -92,9 +40,6 @@ socketLogic(io);
 
 app.use('/api', authRoute, userInfoRoute);
 app.use('/api', chatRoute);
-app.get('/health', (req, res: Response) => {
-  res.status(200).send('OK');
-});
 
 setupSwagger(app, `${CONFIG.BASE_URL}/api`);
 
@@ -102,6 +47,61 @@ server.listen(CONFIG.PORT, () => {
   console.log(`server is running on ${CONFIG.BASE_URL}`);
   console.log(`get api from ${CONFIG.BASE_URL}/api-docs`);
 });
+
+// import express from 'express';
+// import cors from 'cors';
+// import authRoute from './routes/auth.route';
+// import userInfoRoute from './routes/userInfo.route';
+// import chatRoute from './routes/chat.route';
+// import { createServer } from 'node:http';
+// import { Server } from 'socket.io';
+// import { setupSwagger } from './swagger';
+// import { CONFIG } from './config/dotenvConfig';
+// import { socketLogic } from './socketLogic';
+// import { Response } from 'express';
+
+// const app = express();
+
+// const server = createServer(app);
+// export const io = new Server(server, {
+//   cors: {
+//     methods: ['GET', 'POST'],
+//     credentials: true,
+//     // origin: '*',
+//     origin: [
+//       '*',
+//       'wss://192.168.1.7:8001',
+//       'ws://192.168.1.7:8001',
+//       'http://127.0.0.1:5501',
+//       'http://192.168.1.7:8001',
+//       'http://localhost:3000',
+//       'http://127.0.0.1:5500',
+//       'http://127.0.0.1:5501/',
+//       CONFIG.BASE_URL,
+//     ],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   },
+// });
+
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json()); //express default middleware
+
+// app.set('socket', io);
+// socketLogic(io);
+
+// app.use('/api', authRoute, userInfoRoute);
+// app.use('/api', chatRoute);
+// app.get('/health', (req, res: Response) => {
+//   res.status(200).send('OK');
+// });
+
+// setupSwagger(app, `${CONFIG.BASE_URL}/api`);
+
+// server.listen(CONFIG.PORT, () => {
+//   console.log(`server is running on ${CONFIG.BASE_URL}`);
+//   console.log(`get api from ${CONFIG.BASE_URL}/api-docs`);
+// });
 
 // import express from 'express';
 // import http from 'http';
